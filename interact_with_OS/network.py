68d2c3c44ae635fb6283c72170c565560abd6d2a
Andres Ariza <60362951+afarizap@users.noreply.github.com>
2020-11-29 21:53:05 -0500

    return value in check_conectiviy fixed, now it works properly

--- interact_with_OS/network.py
+++ interact_with_OS/network.py
@@ -3,12 +3,12 @@
 import requests
 import socket
 
-def check_localhost:
+def check_localhost():
         """ checks whether the local host is correctly configured """
         localhost = socket.gethostbyname('localhost')
-        return localhost == "123.0.0.1"
+        return localhost == "127.0.0.1"
 
-def check_connectivity:
+def check_connectivity():
         """ checks whether the computer can make successful calls to the intern$
         request = requests.get("http://www.google.com")
-        return request == 200
+        return request.status_code == 200
